fbc7723b94e04df5df28a1e92c33d50f53c89f55
Himal Patel <hpatel@pairsoft.com>
2022-04-10 19:37:03 +1000

    init hangfire.

---
+++ AtomicTransactionDemo/Extension/HangFireExtension.cs
@@ -0,0 +1,27 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+
+namespace AtomicTransactionDemo.Extension;
+
+public static class HangFireExtension
+{
+    public static IServiceCollection RegisterHangFire(this IServiceCollection services, IConfiguration myconfiguration)
+    {
+        services.AddHangfire(configuration => configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSqlServerStorage(myconfiguration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+        {
+            CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+            QueuePollInterval = TimeSpan.Zero,
+            UseRecommendedIsolationLevel = true,
+            DisableGlobalLocks = true
+        }));
+
+        // Add the processing server as IHostedService
+        services.AddHangfireServer();
+        return services;
+    }
+}
--- AtomicTransactionDemo/Program.cs
+++ AtomicTransactionDemo/Program.cs
@@ -1,6 +1,7 @@
 using AtomicTransactionDemo.Core;
 using AtomicTransactionDemo.Extension;
 using AtomicTransactionDemo.Services;
+using Hangfire;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 builder.Services.ConfigureCustomerDbContext(builder.Configuration);
+builder.Services.RegisterHangFire(builder.Configuration);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,8 +29,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseHangfireDashboard();
+
+
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHangfireDashboard();
+});
+BackgroundJob.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+
 
 app.Run();
